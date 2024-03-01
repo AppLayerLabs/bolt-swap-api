6d73f00d2a98ae27c6c50451bb37ad6d7493ab49
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-03-01 13:02:42 -0700

    Add cors

--- server.js
+++ server.js
@@ -2,30 +2,19 @@ require("dotenv").config();
 const loadRouter = require("./routes/loadRouter.js");
 const express = require("express");
 const app = express();
-
-//----- CORS options for API server access -----//
 const cors = require("cors");
 
-// Define an array of allowed domains
-let allowedOrigins;
 
-  allowedOrigins = [
-    "*"
-  ];
-
+// Configure CORS middleware options
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Check if the request origin is in the allowedOrigins array
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+    origin: '*', // This allows all origins. Adjust as necessary for your use case.
+    methods: ['POST', 'GET'], // Allowed request methods
+    allowedHeaders: ['Content-Type', 'Accept-Encoding', 'Accept-Language'], // Allowed request headers
+    credentials: true, // This allows cookies and other credentials to be sent with the request
+    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
 
-// Use the cors middleware with the specified options
-app.use(cors(corsOptions));
+  loadRouter.use(cors(corsOptions));
 //--------------------------------------------------------------------------//
 
 app.use(express.json());
