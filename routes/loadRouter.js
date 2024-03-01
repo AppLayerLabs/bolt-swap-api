c4656a44b6686060c860de5ce954f25d1a84c540
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-03-01 12:53:06 -0700

    Update for metamask http

--- routes/loadRouter.js
+++ routes/loadRouter.js
@@ -6,12 +6,35 @@ const axios = require('axios');
 
 router.post('', async (req, res) => {
     async function fetchChainData() {
+      if (req.method === 'OPTIONS') {
+        res.status(200);
+        res.set({
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, GET',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Accept-Encoding': 'deflate',
+          'Accept-Language': 'en-US'
+        });
+        res.end();
+        return;
+      }
       try {
         let json = JSON.stringify(req.body); 
         if (!json) {
           res.status(500).json({message:"Request is not JSON"})
         }
         const response = await axios.post('http://localhost:8090',req.body);
+        res.set({
+          'Server': 'YourServerName/Version', // Replace with actual server name/version
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, GET',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Content-Type': 'application/json', // This can be conditional based on the request
+          'Connection': 'keep-alive',
+          'Strict-Transport-Security': 'max-age=0',
+          'Vary': 'Origin',
+          'Access-Control-Allow-Credentials': 'true'
+        });
         res.status(200).json(response.data)
       } catch (error) {
         res.status(500).json({message:"Request is not being recieved"})
