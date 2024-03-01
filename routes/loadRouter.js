6d7ea5c1227f7e4f6add2c0a5f88b4b5f9f5cbef
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-03-01 12:31:45 -0700

    Let it  test if it is a json

--- routes/loadRouter.js
+++ routes/loadRouter.js
@@ -7,6 +7,10 @@ const axios = require('axios');
 router.post('', async (req, res) => {
     async function fetchChainData() {
       try {
+        let json = JSON(req.body); 
+        if (!json) {
+          res.status(500).json({message:"Request is not being recieved"})
+        }
         const response = await axios.post('http://localhost:8090',req.body);
         res.status(200).json(response.data)
       } catch (error) {
