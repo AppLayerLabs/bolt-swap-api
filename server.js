d5b08ae0f9558d73700c103198465a95078392fb
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-03-01 12:58:56 -0700

    add cors

--- server.js
+++ server.js
@@ -3,7 +3,33 @@ const loadRouter = require("./routes/loadRouter.js");
 const express = require("express");
 const app = express();
 
+//----- CORS options for API server access -----//
+const cors = require("cors");
+
+// Define an array of allowed domains
+let allowedOrigins;
+
+  allowedOrigins = [
+    "*"
+  ];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the request origin is in the allowedOrigins array
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Use the cors middleware with the specified options
+app.use(cors(corsOptions));
+//--------------------------------------------------------------------------//
+
 app.use(express.json());
+
 
 app.use("/", loadRouter);
 app.listen(3001, () => console.log("Server Started"));
